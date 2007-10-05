5533c44802843052519a9df402f272d776f5cc86
cariaso <cariaso@526b6be0-b13b-0410-8c0e-df8da9501f9f>
2007-10-05 07:20:47 +0000

    seems we need the special version of ncbi

--- mpiblast-build.py
+++ mpiblast-build.py
@@ -9,12 +9,26 @@ def runcmd(cmd):
 	for line in outerr:
 		print line,
 
-runcmd('wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/CURRENT/ncbi.tar.gz')
-runcmd('cvs -d :pserver:anonymous@mpiblast.org:/home/cvs/mpiblast checkout mpiblast')
-runcmd('tar zxvf ncbi.tar.gz')
-runcmd('ln -s ncbi ncbi_c--Mar_12_2007')
-runcmd('patch -p0 < mpiblast/ncbi_Mar2007_evalue.patch')
-runcmd('./ncbi/make/makedis.csh')
+doNcbi = True
+
+
+
+
+if doNcbi:
+
+	#runcmd('wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/CURRENT/ncbi.tar.gz')
+	#runcmd('ln -s ncbi ncbi_c--Mar_12_2007')
+	runcmd('wget ftp://ftp.ncbi.nlm.nih.gov/toolbox/ncbi_tools++/2007/Mar_12_2007/NCBI_C_Toolkit/ncbi_c--Mar_12_2007.tar.gz')
+	runcmd('ln -s ncbi_c--Mar_12_2007 ncbi')
+
+	runcmd('tar zxvf ncbi.tar.gz')
+
+
+	runcmd('cvs -d :pserver:anonymous@mpiblast.org:/home/cvs/mpiblast checkout mpiblast')
+	runcmd('patch -p0 < mpiblast/ncbi_Mar2007_evalue.patch')
+	runcmd('./ncbi/make/makedis.csh')
+
+
 topdir = os.getcwd()
 subdir = 'mpiblast'
 os.chdir(subdir)
@@ -24,4 +38,4 @@ runcmd('automake -a')
 runcmd('autoconf')
 runcmd('./configure --with-ncbi=%s/ncbi' % topdir)
 runcmd('make')
-runcmd('popd')
+os.chdir(topdir)

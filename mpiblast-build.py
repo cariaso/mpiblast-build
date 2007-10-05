159fd065abdbfa0b5a5d360c0905b06fcc41d27d
cariaso <cariaso@526b6be0-b13b-0410-8c0e-df8da9501f9f>
2007-10-05 05:01:21 +0000

    slightly real

--- mpiblast-build.py
+++ mpiblast-build.py
@@ -6,7 +6,22 @@ import string, os
 
 def runcmd(cmd):
         (inf, outerr) = os.popen4(cmd)
-	for line in outerr.readlines():
-		print line
+	for line in outerr:
+		print line,
 
 runcmd('wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/CURRENT/ncbi.tar.gz')
+runcmd('cvs -d :pserver:anonymous@mpiblast.org:/home/cvs/mpiblast checkout mpiblast')
+runcmd('tar zxvf ncbi.tar.gz')
+runcmd('ln -s ncbi ncbi_c--Mar_12_2007')
+runcmd('patch -p0 < mpiblast/ncbi_Mar2007_evalue.patch')
+runcmd('./ncbi/make/makedis.csh')
+topdir = os.getcwd()
+subdir = 'mpiblast'
+os.chdir(subdir)
+runcmd('aclocal')
+runcmd('autoheader')
+runcmd('automake -a')
+runcmd('autoconf')
+runcmd('./configure --with-ncbi=%s/ncbi' % topdir)
+runcmd('make')
+runcmd('popd')

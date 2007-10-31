a36ff44fed9e504b38789eb4d397a0e5f4da681e
cariaso <cariaso@526b6be0-b13b-0410-8c0e-df8da9501f9f>
2007-10-31 02:44:36 +0000

    slightly better-er

--- mpiblast-build.py
+++ mpiblast-build.py
@@ -4,15 +4,26 @@
 import string, os
 
 
-def runcmd(cmd):
-        (inf, outerr) = os.popen4(cmd)
+def runcmd(cmd, cd='.'):
+        (inf, outerr) = os.popen4('cd %s; %s ' % (cd, cmd))
 	for line in outerr:
 		print line,
 
-doNcbi = True
+def ask(msg):
+	resp = raw_input('%s [y/N]:? ' % msg)
+	try:
+		if resp.lower()[0] == 'y':
+			return True
+	except:
+		pass
+
+	return False
+	
 
 import optparse
 import sys
+
+
 
 def main():
 	# Set up our options
@@ -46,29 +57,49 @@ def main():
 				 action='store_false',
 				 help='Do not show progress',
 				 )
-	
 
 	(options, args) = option_parser.parse_args()
-	print 'options=%s' % options
-	print 'args=%s' % args
 
-        if options.doDownloadNcbi:
- 	    #runcmd('wget ftp://ftp.ncbi.nih.gov/toolbox/ncbi_tools/CURRENT/ncbi.tar.gz')
-	    #runcmd('ln -s ncbi ncbi_c--Mar_12_2007')
-
-	    runcmd('wget ftp://ftp.ncbi.nlm.nih.gov/toolbox/ncbi_tools++/2007/Mar_12_2007/NCBI_C_Toolkit/ncbi_c--Mar_12_2007.tar.gz')
-	    runcmd('ln -s ncbi_c--Mar_12_2007 ncbi')
-	    runcmd('ln -s ncbi_c--Mar_12_2007.tar.gz ncbi.tar.gz')
+	if ask('download ncbi'):
+		runcmd('wget ftp://ftp.ncbi.nlm.nih.gov/toolbox/ncbi_tools++/2007/Mar_12_2007/NCBI_C_Toolkit/ncbi_c--Mar_12_2007.tar.gz')
 
 
-        if options.doNcbi:
+	if ask('untar ncbi'):
 
-	    runcmd('tar zxvf ncbi.tar.gz')
-	    runcmd('cvs -d :pserver:anonymous@mpiblast.org:/home/cvs/mpiblast checkout mpiblast')
-	    runcmd('patch -p0 < mpiblast/ncbi_Mar2007_evalue.patch')
-	    runcmd('./ncbi/make/makedis.csh')
+		runcmd('ln -s ncbi_c--Mar_12_2007 ncbi')
+		runcmd('tar zxvf ncbi_c--Mar_12_2007.tar.gz')
 
 
+	if ask('checkout mpiblast'):
+
+		runcmd('cvs -d :pserver:anonymous@mpiblast.org:/home/cvs/mpiblast checkout mpiblast')
+
+
+	if ask('patch ncbi'):
+
+		runcmd('patch -p0 < mpiblast/ncbi_Mar2007_evalue.patch')
+
+	if ask('make ncbi'):
+		runcmd('./ncbi/make/makedis.csh')
+
+
+	subdir='mpiblast'
+
+
+	if ask('prep mpiblast'):
+		runcmd('aclocal', cd=subdir)
+		runcmd('autoheader', cd=subdir)
+		runcmd('automake -a', cd=subdir)
+		runcmd('autoconf', cd=subdir)
+
+
+	if ask('configure mpiblast'):
+		topdir = os.getcwd()
+		runcmd('./configure --with-ncbi=%s/ncbi' % topdir, cd=subdir)
+
+	if ask('make mpiblast'):
+		runcmd('make', cd=subdir)
+	
 
 
 
@@ -76,14 +107,3 @@ def main():
 main()
 sys.exit()
 
-
-topdir = os.getcwd()
-subdir = 'mpiblast'
-os.chdir(subdir)
-runcmd('aclocal')
-runcmd('autoheader')
-runcmd('automake -a')
-runcmd('autoconf')
-runcmd('./configure --with-ncbi=%s/ncbi' % topdir)
-runcmd('make')
-os.chdir(topdir)
